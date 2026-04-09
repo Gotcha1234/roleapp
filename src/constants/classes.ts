d42cd70a298118894a7a4080688d8380b90d976a
Gotcha123x <carlos.fernandez@triagons.com>
2026-04-09 13:27:07 -0700

    [feat]: add new files with constants objects

---
+++ src/constants/classes.ts
@@ -0,0 +1,14 @@
+export const CLASSES = {
+    PALADIN: 'paladin',
+    WIZARD: 'wizard',
+    RANGER: 'ranger',
+    WARRIOR: 'warrior',
+    ROGUE: 'rogue',
+    MONK: 'monk',
+    DRUID: 'druid',
+    // These are not defined in the original list but added for completeness
+    CLERIC: 'cleric',
+    BARD: 'bard',
+    SORCERER: 'sorcerer',
+    BARBARIAN: 'barbarian',
+};
