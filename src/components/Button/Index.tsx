f529b9cc14151fa48efe8338ea939d61487a1a7b
Efren <efrencast04@hotmail.com>
2026-04-10 20:24:50 -0700

    feat: created button component, added to App

--- src/components/Button/Index.tsx
+++ src/components/Button/Index.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import styles from './styled'
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+import styles from './styled';
 
-function Button() {
+//Props for the Button component (label, onPress function, variant types, and optional disabled state)
+type Props = {
+  label: string;
+  onPress: () => void;
+  variant: 'primary' | 'secondary' | 'ghost';
+  disabled?: boolean;
+};
+
+//Button function plus constants to determine button style and text style
+function Button({ label, onPress, variant, disabled = false }: Props) {
+  const buttonVariantStyle = styles[variant];
+  const textVasrianStyle = variant === 'primary' ? styles.primaryText : styles.secondaryText;
+
   return (
-    <div style={styles.container}>Button</div>
-  )
+    <Pressable
+      onPress={onPress}
+      disabled={disabled}
+      style={[styles.button, buttonVariantStyle, disabled && styles.disabledButton]}
+    >
+      <Text style={[styles.text, textVasrianStyle, disabled && styles.disabledText]}>
+        {label}
+      </Text>
+    </Pressable>
+  );
 }
 
-export default Button
+export default Button;
