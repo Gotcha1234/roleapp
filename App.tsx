4b9e8cef0cd36b0d22266067274eac1fa7674188
Efren <efrencast04@hotmail.com>
2026-04-10 20:40:25 -0700

    fix: delete extra empty line

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './src/components/Button';
 
-
 export default function App() {
   return (
     <View style={styles.container}>
