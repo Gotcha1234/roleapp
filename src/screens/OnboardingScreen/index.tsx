c3111332cba4788bb88dcc2ffd92b3dafe98446d
Gotcha123x <carlos.fernandez@triagons.com>
2026-04-11 00:04:07 -0700

    [fix]: rename onboarding screen to OnboardingScreen to match architecture planning

--- src/screens/OnboardingScreen/index.tsx
+++ src/screens/OnboardingScreen/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export const Onboarding = () => {
+export const OnboardingScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Onboarding</Text>
