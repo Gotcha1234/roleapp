8aab74d40724e9a83e5ec9519e3ad00eea92bfd5
Gotcha123x <carlostibea@hotmail.com>
2026-04-14 23:36:46 -0700

    [feat]: load Inter fonts in App entry point

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <SafeAreaProvider>
       <NavigationContainer>
