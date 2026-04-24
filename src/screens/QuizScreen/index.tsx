d228967523657f37fece228052178fee01ed2a69
Gotcha123x <carlostibea@hotmail.com>
2026-04-23 22:15:40 -0700

    [feat]: add navigation buttons for quiz screen and add chip label colors

--- src/screens/QuizScreen/index.tsx
+++ src/screens/QuizScreen/index.tsx
@@ -5,24 +5,64 @@ import { createStyles } from './styled';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Chip } from '../../components/Chip';
 import { COLORS } from '../../constants/theme';
-import { loadQuizAnswers, loadQuizProgress } from '../../store/quizStorage';
+import { loadQuizAnswers, loadQuizProgress } from '../../store/quizStore';
 import { allQuizQuestions } from '../../constants/quizQuestions';
 import { QuizOption } from '../../components/QuizOption';
+import { statNames, STATS } from '../../constants/stats';
+import Button from '../../components/Button';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { SCREENS } from '../../constants/constants';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../navigation/AppNavigator';
+
+const CHIP_LABEL = {
+  [STATS.STRENGTH]: 'Fuerza — Ejercicio de fuerza',
+  [STATS.INTELLIGENCE]: 'Inteligencia — Ejercicio de inteligencia',
+  [STATS.ENDURANCE]: 'Resistencia — Ejercicio de resistencia',
+  [STATS.WISDOM]: 'Sabiduría — Ejercicio de sabiduría',
+  [STATS.FAITH]: 'Fe — Ejercicio de fe',
+  [STATS.MAGIC]: 'Magia — Ejercicio de magia',
+};
+
+const COLORS_QUIZ = {
+  [STATS.STRENGTH]: COLORS.QUIZ_STRENGTH,
+  [STATS.INTELLIGENCE]: COLORS.QUIZ_INTELLIGENCE,
+  [STATS.ENDURANCE]: COLORS.QUIZ_ENDURANCE,
+  [STATS.WISDOM]: COLORS.QUIZ_WISDOM,
+  [STATS.FAITH]: COLORS.QUIZ_FAITH,
+  [STATS.MAGIC]: COLORS.QUIZ_MAGIC,
+};
+
+type Nav = NativeStackNavigationProp<RootStackParamList, typeof SCREENS.onboardingIntro>;
 
 export const QuizScreen = () => {
   const insets = useSafeAreaInsets();
-  const styles = createStyles({ top: insets.top, bottom: insets.bottom });
   const [step, setStep] = useState(1);
   const [answers, setAnswers] = useState<{ [key: string]: string }>({});
+  const navigation = useNavigation<Nav>();
+  const currentQuestion = allQuizQuestions[step - 1]
+  const statName = statNames[currentQuestion.stat];
+  const totalAnsweredQuestions = Object.keys(answers).length;
+  const styles = createStyles({ top: insets.top, bottom: insets.bottom, chipBgColor: COLORS_QUIZ[currentQuestion.stat] });
 
-  const handleAnswer = (questionId: string, optionKey: string) => {
-    console.log('Selected answer ==> :', {
-      questionId, optionKey
-    });
-    setAnswers(prev => ({ ...prev, [questionId]: optionKey }));
+  const handlePreviousStep = () => {
+    if (step === 1) {
+      navigation.navigate(SCREENS.onboardingIntro);
+    } else {
+      setStep(step - 1);
+    }
   };
 
-  const currentQuestion = allQuizQuestions[step - 1]
+  const handleNextStep = () => {
+    if (step < allQuizQuestions.length) {
+      setStep(step + 1);
+    }
+  };
+
+  const handleAnswer = (questionId: string, optionKey: string) => {
+    setAnswers(prev => ({ ...prev, [questionId]: optionKey }));
+  };
 
   useEffect(() => {
     const restoreQuizData = async () => {
@@ -39,13 +79,16 @@ export const QuizScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Stepper currentStep={step} totalSteps={allQuizQuestions.length} label={currentQuestion.stat} />
-      <Chip
-        label={currentQuestion.stat}
-        backgroundColor={COLORS.CHIP_BG}
-        borderColor={COLORS.BG_COMPONENTS}
-        labelColor={COLORS.TEXT_DIM}
-      />
+      <Stepper currentStep={step} totalSteps={allQuizQuestions.length} label={statName} />
+      <View style={styles.chipContainer}>
+        <Chip
+          icon={<View style={styles.chipIcon} />}
+          label={CHIP_LABEL[currentQuestion.stat]}
+          backgroundColor={COLORS.CHIP_BG}
+          borderColor={COLORS.BG_COMPONENTS}
+          labelColor={COLORS.TEXT_DIM}
+        />
+      </View>
       <View>
         <Text style={styles.questionText}>{currentQuestion.question}</Text>
         <View style={styles.optionsContainer}>
@@ -60,6 +103,24 @@ export const QuizScreen = () => {
             />
           ))}
         </View>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button
+          label={step === 1 ? 'Atrás' : 'Anterior'}
+          onPress={handlePreviousStep}
+          variant='secondary'
+          minWidth={80}
+          iconLeft={<Ionicons name="arrow-back" size={20} color={COLORS.TEXT_DIM} />}
+        />
+        <Button
+          label="Siguiente"
+          onPress={handleNextStep}
+          disabled={totalAnsweredQuestions < allQuizQuestions.length && !answers[currentQuestion.id]}
+          variant='primary'
+          iconRight={<Ionicons name="arrow-forward" size={20} color={COLORS.BLACK_DARK} />}
+          minWidth={80}
+          maxWidth={160}
+        />
       </View>
     </View>
   );
--- src/screens/QuizScreen/styled.ts
+++ src/screens/QuizScreen/styled.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { COLORS } from '../../constants/theme';
 
-export const createStyles = ({ top, bottom }: { top: number, bottom: number }) => StyleSheet.create({
+export const createStyles = ({ top, bottom, chipBgColor }: { top: number, bottom: number, chipBgColor: string }) => StyleSheet.create({
+  chipContainer: {
+    marginVertical: 16,
+  },
   container: {
     flex: 1,
     justifyContent: 'flex-start',
@@ -18,5 +21,20 @@ export const createStyles = ({ top, bottom }: { top: number, bottom: number }) =
   },
   optionsContainer: {
     gap: 10,
+    marginBottom: 24
+  },
+  buttonsContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    gap: 16
+  },
+  chipIcon: {
+    width: 8,
+    height: 8,
+    borderRadius: 9999,
+    backgroundColor: chipBgColor,
   },
 });
