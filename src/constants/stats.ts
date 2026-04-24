8cfa8666f28f88f6988607fa71738867026e0fdd
Gotcha123x <carlostibea@hotmail.com>
2026-04-23 22:13:37 -0700

    [feat]: add stat names to constants

--- src/constants/stats.ts
+++ src/constants/stats.ts
@@ -1,18 +1,27 @@
 export const STATS = {
-    STRENGTH: 'strength',
-    INTELLIGENCE: 'intelligence',
-    ENDURANCE: 'endurance',
-    WISDOM: 'wisdom',
-    FAITH: 'faith',
-    MAGIC: 'magic',
+  STRENGTH: 'strength',
+  INTELLIGENCE: 'intelligence',
+  ENDURANCE: 'endurance',
+  WISDOM: 'wisdom',
+  FAITH: 'faith',
+  MAGIC: 'magic',
+};
+
+export const statNames = {
+  [STATS.STRENGTH]: 'Fuerza',
+  [STATS.INTELLIGENCE]: 'Inteligencia',
+  [STATS.ENDURANCE]: 'Resistencia',
+  [STATS.WISDOM]: 'Sabiduría',
+  [STATS.FAITH]: 'Fe',
+  [STATS.MAGIC]: 'Magia',
 };
 
 // TODO: Maybe move these icons to assets folder when they are ready from designer
 export const icons = {
-    [STATS.STRENGTH]: '🗡️',
-    [STATS.INTELLIGENCE]: '📚',
-    [STATS.ENDURANCE]: '🏃',
-    [STATS.WISDOM]: '🎯',
-    [STATS.FAITH]: '✨',
-    [STATS.MAGIC]: '🔮',
+  [STATS.STRENGTH]: '🗡️',
+  [STATS.INTELLIGENCE]: '📚',
+  [STATS.ENDURANCE]: '🏃',
+  [STATS.WISDOM]: '🎯',
+  [STATS.FAITH]: '✨',
+  [STATS.MAGIC]: '🔮',
 };
